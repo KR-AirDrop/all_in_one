1f4d5f7751f9d0163e6355692b9a11df5f02ca65
KR-AirDrop <choish4682@naver.com>
2022-02-13 21:27:05 +0900

    products_page_set

--- hargi_market_web/src/main/index.js
+++ hargi_market_web/src/main/index.js
@@ -21,47 +21,35 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header_area">
-          <img src="images/icons/logo.png" alt="~" />
-        </div>
+      <div id="banner">
+        <img src="/images/banners/banner1.png" alt="~" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" alt="~" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product_list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product_card">
-                <Link className="product_link" to={"/products/" + index}>
-                  <div>
+      <h1>판매되는 상품들</h1>
+      <div id="product_list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product_card">
+              <Link className="product_link" to={`/products/${product.id}`}>
+                <div>
+                  <img className="product_img" src={product.imageURL} alt="~" />
+                </div>
+                <div className="product_contents">
+                  <span className="product_name">{product.name}</span>
+                  <span className="product_price">{product.price}원</span>
+                  <div className="product_seller">
                     <img
-                      className="product_img"
-                      src={product.imageURL}
+                      className="product_avatar"
+                      src="images/icons/avatar.png"
                       alt="~"
                     />
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="product_contents">
-                    <span className="product_name">{product.name}</span>
-                    <span className="product_price">{product.price}원</span>
-                    <div className="product_seller">
-                      <img
-                        className="product_avatar"
-                        src="images/icons/avatar.png"
-                        alt="~"
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>;
     </div>
   );
 }
---
+++ hargi_market_web/src/product/index.js
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
+
+function ProductPage() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://bc61c99d-22e2-4114-9055-41afc6ae387b.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image_box">
+        <img src={"/" + product.imageURL} />
+      </div>
+      <div id="profile_box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents_box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022년 02월 13일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
